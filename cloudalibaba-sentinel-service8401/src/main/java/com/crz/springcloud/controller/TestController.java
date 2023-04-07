29a2ca10716cade92b33e737af21126e21b5df27
chenruizhou1 <1259522407@qq.com>
2023-04-07 18:05:23 +0800

    C

--- cloudalibaba-sentinel-service8401/src/main/java/com/crz/springcloud/controller/TestController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/com/crz/springcloud/controller/TestController.java
@@ -1,6 +1,9 @@
 package com.crz.springcloud.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,6 +17,16 @@ public class TestController {
     @GetMapping("/test2")
     public String test2() {
         return "Hello Sentinel ==> test2";
+    }
+
+    @GetMapping("/testPfc")
+    @SentinelResource(value = "testPfc", blockHandler = "testPfc_handler")
+    public String testPfc(@RequestParam(value = "p1", required = false) String p1,
+                          @RequestParam(value = "p2", required = false) String p2) {
+        return "You succeed";
+    }
+    public String testPfc_handler(String p1, String p2, BlockException ex) {
+        return "You've been restricted (￣▽￣)";
     }
 
 }
